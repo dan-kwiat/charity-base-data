2d0cd15b976517319e75b2a4db2822210a761117
dan-kwiat <dan.kwiat@gmail.com>
2020-05-19 15:54:47 +0100

    feat(import-social-avatar): make s3 logo objects public so cloudfront can read them

--- import-social-avatar/upload-data.js
+++ import-social-avatar/upload-data.js
@@ -1,21 +1,20 @@
-const s3 = require('./lib/s3')
+const s3 = require("./lib/s3")
 
-const {
-  S3_BUCKET,
-  S3_PATH,
-} = process.env
+const { S3_BUCKET, S3_PATH } = process.env
 
 const upload = async ({ id, size, body }) => {
   try {
-
     const bucket = S3_BUCKET
     const key = `${S3_PATH}/${size}/${id}`
 
-    await s3.upload({
-      Bucket: bucket,
-      Key: key,
-      Body: body,
-    }).promise()
+    await s3
+      .upload({
+        ACL: "public-read",
+        Bucket: bucket,
+        Key: key,
+        Body: body,
+      })
+      .promise()
 
     return {
       id,
@@ -23,7 +22,7 @@ const upload = async ({ id, size, body }) => {
       bucket,
       key,
     }
-  } catch(e) {
+  } catch (e) {
     log.error(`Failed to upload image: '${id}'`)
     log.error(e)
     return null
