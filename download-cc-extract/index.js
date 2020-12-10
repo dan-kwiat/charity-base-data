a644de8717b12aaa4227e5f513be4c179761786d
dan-kwiat <dan.kwiat@gmail.com>
2020-12-10 10:52:22 +0000

    feat(download-cc): update MB_TOTAL_ESTIMATE

--- download-cc-extract/index.js
+++ download-cc-extract/index.js
@@ -1,20 +1,20 @@
-require('dotenv').config()
-const { pipeline } = require('stream')
-const fetch = require('node-fetch')
-const unzipper = require('unzipper')
-const getProgressBar = require('./lib/progress')
-const log = require('./lib/logger')
+require("dotenv").config()
+const { pipeline } = require("stream")
+const fetch = require("node-fetch")
+const unzipper = require("unzipper")
+const getProgressBar = require("./lib/progress")
+const log = require("./lib/logger")
 
-const MB_TOTAL_ESTIMATE = 141
+const MB_TOTAL_ESTIMATE = 145
 const MB_PROGRESS_STEP = 1
 
-const downloadUnzip = (url, path) => (
+const downloadUnzip = (url, path) =>
   new Promise(async (resolve, reject) => {
     if (!url) {
-      reject('Missing zipped data URL')
+      reject("Missing zipped data URL")
     }
     if (!path) {
-      reject('Missing download destination')
+      reject("Missing download destination")
     }
 
     let bytes = 0
@@ -24,35 +24,30 @@ const downloadUnzip = (url, path) => (
     try {
       log.info(`Downloading & unzipping data to '${path}'`)
       res = await fetch(url)
-      progressBar = getProgressBar('Download Progress (MB)')
+      progressBar = getProgressBar("Download Progress (MB)")
       progressBar.start(MB_TOTAL_ESTIMATE, 0)
-      res.body.on("data", chunk => {
+      res.body.on("data", (chunk) => {
         bytes += chunk.length
-        const step = Math.floor(bytes/(1000000*MB_PROGRESS_STEP))
+        const step = Math.floor(bytes / (1000000 * MB_PROGRESS_STEP))
         if (step > i) {
-          progressBar.update(Math.round(bytes/1000000))
+          progressBar.update(Math.round(bytes / 1000000))
           i = step
         }
       })
-    } catch(e) {
+    } catch (e) {
       reject(e)
     }
 
-    pipeline(
-      res.body,
-      unzipper.Extract({ path }),
-      (err) => {
-        if (err) {
-          reject(err)
-        } else {
-          progressBar.update(Math.round(bytes/1000000))
-          progressBar.stop()
-          resolve(bytes)
-        }
+    pipeline(res.body, unzipper.Extract({ path }), (err) => {
+      if (err) {
+        reject(err)
+      } else {
+        progressBar.update(Math.round(bytes / 1000000))
+        progressBar.stop()
+        resolve(bytes)
       }
-    )
+    })
   })
-)
 
 async function f() {
   try {
@@ -60,7 +55,7 @@ async function f() {
       process.env.CC_ZIPPED_DATA_URL,
       process.env.CC_DOWNLOAD_DESTINATION
     )
-  } catch(e) {
+  } catch (e) {
     log.error(e)
     process.exit(1)
   }
