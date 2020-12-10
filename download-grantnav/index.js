903b18d5bc0408746eb00e231bbaf0aecd187ad4
dan-kwiat <dan.kwiat@gmail.com>
2020-12-10 10:53:53 +0000

    feat(download-grantnav): update MB_TOTAL_ESTIMATE

--- download-grantnav/index.js
+++ download-grantnav/index.js
@@ -1,27 +1,29 @@
-require('dotenv').config()
-const { pipeline } = require('stream')
-const fs = require('fs')
-const fetch = require('node-fetch')
-const getProgressBar = require('./lib/progress')
-const log = require('./lib/logger')
+require("dotenv").config()
+const { pipeline } = require("stream")
+const fs = require("fs")
+const fetch = require("node-fetch")
+const getProgressBar = require("./lib/progress")
+const log = require("./lib/logger")
 
-const MB_TOTAL_ESTIMATE = 301
+const MB_TOTAL_ESTIMATE = 391
 const MB_PROGRESS_STEP = 1
 
-const download = (url, path, fileName) => (
+const download = (url, path, fileName) =>
   new Promise(async (resolve, reject) => {
     if (!url) {
-      reject('Missing data URL')
+      reject("Missing data URL")
     }
     if (!path) {
-      reject('Missing download destination')
+      reject("Missing download destination")
     }
 
     try {
-      await fs.promises.mkdir([__dirname, path].join('/'), { recursive: true })
+      await fs.promises.mkdir([__dirname, path].join("/"), { recursive: true })
     } catch (e) {}
 
-    const writeStream = fs.createWriteStream([__dirname, path, fileName].join('/'))
+    const writeStream = fs.createWriteStream(
+      [__dirname, path, fileName].join("/")
+    )
 
     let bytes = 0
     let i = 0
@@ -30,35 +32,30 @@ const download = (url, path, fileName) => (
     try {
       log.info(`Downloading data to '${path}'`)
       res = await fetch(url)
-      progressBar = getProgressBar('Download Progress (MB)')
+      progressBar = getProgressBar("Download Progress (MB)")
       progressBar.start(MB_TOTAL_ESTIMATE, 0)
-      res.body.on("data", chunk => {
+      res.body.on("data", (chunk) => {
         bytes += chunk.length
-        const step = Math.floor(bytes/(1000000*MB_PROGRESS_STEP))
+        const step = Math.floor(bytes / (1000000 * MB_PROGRESS_STEP))
         if (step > i) {
-          progressBar.update(Math.round(bytes/1000000))
+          progressBar.update(Math.round(bytes / 1000000))
           i = step
         }
       })
-    } catch(e) {
+    } catch (e) {
       reject(e)
     }
 
-    pipeline(
-      res.body,
-      writeStream,
-      (err) => {
-        if (err) {
-          reject(err)
-        } else {
-          progressBar.update(Math.round(bytes/1000000))
-          progressBar.stop()
-          resolve(bytes)
-        }
+    pipeline(res.body, writeStream, (err) => {
+      if (err) {
+        reject(err)
+      } else {
+        progressBar.update(Math.round(bytes / 1000000))
+        progressBar.stop()
+        resolve(bytes)
       }
-    )
+    })
   })
-)
 
 async function f() {
   try {
@@ -67,7 +64,7 @@ async function f() {
       process.env.GRANTNAV_DOWNLOAD_DESTINATION,
       process.env.GRANTNAV_FILENAME
     )
-  } catch(e) {
+  } catch (e) {
     log.error(e)
     process.exit(1)
   }
